b5dacb51cb394c8b7511a892b04d4f500e6f8568
vladimir <52894331+vladi2909@users.noreply.github.com>
2021-05-30 13:46:47 +0300

    feat: add jsdoc for services

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,13 +1,33 @@
 const usersRepo = require('./user.memory.repository');
-
+/**
+ * Gets all users
+ * @returns {Promise<Array>} returns coppied array of all users
+ */
 const getAll = () => usersRepo.getAll();
-
+/**
+ * Finds a user by id, if there is no user with that id, an error is returned
+ * @param {string} id the user id of which to find
+ * @returns {Promise<Object>} returns the user if there is such an id
+ */
 const get = id => usersRepo.get(id);
-
+/**
+ * Create a new user in the Database
+ * @param {Object} user new user to create
+ * @returns {Promise<Object>} returns the created user
+ */
 const create = user => usersRepo.create(user);
-
+/**
+ * Delete user by id from Database
+ * @param {string} id the id of user to be deleted
+ * @returns {Promise<Object>} returns the deleted user
+ */
 const delById = id => usersRepo.delById(id);
-
+/**
+ * Update user in the Database, if there is no user with that id, an error is returned
+ * @param {string} id the user id of which to find and update
+ * @param {Object} modUser the user to be changed
+ * @returns {Promise<Object>} updated user
+ */
 const update = (id, modUser) => usersRepo.update(id, modUser);
 
 module.exports = { getAll, get, create, delById, update };
