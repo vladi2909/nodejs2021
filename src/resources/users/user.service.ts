48ef4311ef60076ea435e303a2100d8eba6d1e4a
vladimir <52894331+vladi2909@users.noreply.github.com>
2021-06-09 23:28:29 +0300

    Revert "fix: change types"
    This reverts commit aab01d5a6cbe770c68159fb3b456413c3ee6da2a.

--- src/resources/users/user.service.ts
+++ src/resources/users/user.service.ts
@@ -1,45 +1,56 @@
 import { IUser } from '../../models/user.model';
+
 const usersRepo = require('./user.memory.repository');
 
 /**
- * Gets all users
+ * Shows all users
+ *
  * @async
- * @return {Promise<User[]>} returns a new array of all users.
+ * @function getAll
+ * @return {Promise<array>} returns a new array containing copies of all users.
  */
-const getAll = (): Promise<IUser[]> => usersRepo.getAll();
+const getAll = (): Promise<Array<IUser>> => usersRepo.getAll();
 
 /**
- * Finds a user by id
+ * Finds a user in the database by ID. If there is no user with this ID, it returns an error.
+ *
  * @async
- * @param {string} id the user id
- * @return {Promise<User>} returns the user if there is such an id
+ * @function get
+ * @param {String} id the user you want to find
+ * @return {Promise<Object>} returns the user with the specified id
  */
 const get = (id: string): Promise<IUser> => usersRepo.get(id);
 
 /**
- * Add a new user.
+ * Adds a new user to the database.
+ *
  * @async
+ * @function create
  * @param {Object} new user
- * @return {Promise<User>} returns the user
+ * @return {Promise<Object>} returns the user who was created
  */
 const create = (user: IUser): Promise<IUser> => usersRepo.create(user);
 
 /**
- * remove user.
+ * A user by ID and removes it from the database. And finds all tasks associated with this user and changes the userId to null. If there is no user with this ID, it returns an error.
+ *
  * @async
- * @param {String} id user id
- * @return {Promise<User>} returns the deleted user
+ * @function deleteById
+ * @param {String} id the user you want to find
+ * @return {Promise<Object>} returns the deleted user
  */
 const deleteById = (id: string): Promise<IUser> => usersRepo.deleteById(id);
 
 /**
- * Update user
+ * Finds a user in the database and edits it. If there is no user with this ID, it returns an error.
+ *
  * @async
- * @param {String} id user id
- * @param {Partial<User>} edited user
- * @return {Promise<User>} edited user
+ * @function update
+ * @param {String} id the user you want to find
+ * @param {Object} edited user
+ * @return {Promise<Object>} edited user
  */
-const update = (id: string, modUser: IUser): Promise<IUser> =>
-  usersRepo.update(id, modUser);
+const update = (id: string, modifiedUser: IUser): Promise<IUser> =>
+  usersRepo.update(id, modifiedUser);
 
 module.exports = { getAll, get, create, deleteById, update };
