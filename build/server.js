bb751c3fd6f48e9d3ef5b47a4b4209def8131661
vladimir <52894331+vladi2909@users.noreply.github.com>
2021-06-09 22:30:54 +0300

    feat: delete build & doc

--- build/server.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const app = require('./app');
-const { PORT } = require('./common/config');
-app.listen(PORT, () => process.stdout.write(`App is running on http://localhost:${PORT}`));
