23bbb67eee5bdb9e327b009ea87397e3cef038ba
vladimir <52894331+vladi2909@users.noreply.github.com>
2021-06-10 11:08:39 +0300

    fix: change types in db

--- src/common/DB.ts
+++ src/common/DB.ts
@@ -2,32 +2,33 @@ import { IUser } from '../models/user.model';
 import { IBoard } from '../models/board.model';
 import { ITask } from '../models/task.model';
 import { IDB } from '../models/db.model';
+import { Board } from '../resources/boards/board.model';
 
 export const DB: IDB = {
   users: [],
   boards: [],
-  tasks: [],
+  tasks: []
 };
 
 /**
  * Gets all users.
  * @returns {Promise<User[]>} returns array of all users
  */
-export const getAllUsers = async (): Promise<Array<object>> => DB.users.slice(0);
+export const getAllUsers = async (): Promise<IUser[]> => DB.users.slice(0);
 
 /**
  * Gets user by id.
  * @param {string} id the user id
  * @returns {Promise<User>} returns the user if there is such an id
  */
-export const getUser = async (id: string): Promise<object | undefined> => DB.users.filter(item => item.id === id)[0];
+export const getUser = async (id: string): Promise<IUser | undefined> => DB.users.filter(item => item.id === id)[0];
 
 /**
  * Add a new user.
  * @param {object} new user
  * @returns {Promise<User>} returns the user
  */
-export const createUser = async (user: IUser): Promise<object> => {
+export const createUser = async (user: IUser): Promise<IUser> => {
   DB.users.push(user);
   return user;
 };
@@ -37,15 +38,15 @@ export const createUser = async (user: IUser): Promise<object> => {
  * @param {string} id the user id
  * @returns {Promise<User>} returns the deleted user
  */
-export const deleteUser = async (id: string): Promise<object | boolean> => {
-  const forDeletion = DB.users.filter(item => item.id === id)[0];
+export const deleteUser = async (id: string): Promise<IUser | null> => {
+  const deletion = DB.users.filter(item => item.id === id)[0];
 
-  if (!forDeletion) {
-    return false;
+  if (!deletion) {
+    return null;
   }
 
-  DB.users = DB.users.filter(user => user.id !== forDeletion.id);
-  return forDeletion;
+  DB.users = DB.users.filter(user => user.id !== deletion.id);
+  return deletion;
 };
 
 /**
@@ -54,7 +55,7 @@ export const deleteUser = async (id: string): Promise<object | boolean> => {
  * @param {Object} edited user
  * @returns {Promise<User>}  edited user
  */
-export const updateUser = async (id: string, modUser: IUser): Promise<object | null> => {
+export const updateUser = async (id: string, modUser: IUser): Promise<IUser | null> => {
   const user = DB.users.filter(item => item.id === id)[0];
 
   if (!user) {
@@ -76,21 +77,21 @@ export const updateUser = async (id: string, modUser: IUser): Promise<object | n
  * Gets all boards.
  * @returns {Promise<Board[]>} returns a new array of all boards
  */
-export const getAllBoards = async (): Promise<Array<object>> => DB.boards.slice(0);
+export const getAllBoards = async (): Promise<Board[]> => DB.boards.slice(0);
 
 /**
  * get board by id.
  * @param {string} id the board id
  * @returns {Promise<Board>} if there is such an id, it returns the board with this id
  */
-export const getBoard = async (id: string): Promise<object | undefined> => DB.boards.filter(item => item.id === id)[0];
+export const getBoard = async (id: string): Promise<IBoard | undefined> => DB.boards.filter(item => item.id === id)[0];
 
 /**
  * Add a new board.
  * @param {object} new board
  * @returns {Promise<Board>} returns the board
  */
-export const createBoard = async (board: IBoard): Promise<object> => {
+export const createBoard = async (board: IBoard): Promise<Board> => {
   DB.boards.push(board);
   return board;
 };
@@ -100,11 +101,11 @@ export const createBoard = async (board: IBoard): Promise<object> => {
  * @param {string} id board id
  * @returns {Promise<Board>} returns the deleted board
  */
-export const deleteBoard = async (id: string): Promise<object | boolean> => {
+export const deleteBoard = async (id: string): Promise<Board | null> => {
   const deletion = DB.boards.filter(item => item.id === id)[0];
 
   if (!deletion) {
-    return false;
+    return null;
   }
 
   DB.boards = DB.boards.filter(board => board.id !== deletion.id);
@@ -117,11 +118,11 @@ export const deleteBoard = async (id: string): Promise<object | boolean> => {
  * @param {Board} edited board
  * @returns {Promise<Board>} edited board
  */
-export const updateBoard = async (id: string, modBoard: IBoard): Promise<object | boolean> => {
+export const updateBoard = async (id: string, modBoard: IBoard): Promise<Board | null> => {
   const board = DB.boards.filter(item => item.id === id)[0];
 
   if (!board) {
-    return false;
+    return null;
   }
 
   DB.boards = DB.boards.map(item => {
@@ -137,21 +138,21 @@ export const updateBoard = async (id: string, modBoard: IBoard): Promise<object 
  * Gets all tasks
  * @returns {Promise<Task[]>} returns a new array of all tasks
  */
-export const getAllTasksByBoard = async (): Promise<Array<object>> => DB.tasks.slice(0);
+export const getAllTasksByBoard = async (): Promise<ITask[]> => DB.tasks.slice(0);
 
 /**
  * get a task by id.
  * @param {string} id the task
  * @returns {Promise<Taks>}if there is such an id, it returns the task with this id
  */
-export const getTask = async (id: string): Promise<object | undefined> => DB.tasks.filter(item => item.id === id)[0];
+export const getTask = async (id: string): Promise<ITask | undefined> => DB.tasks.filter(item => item.id === id)[0];
 
 /**
  * Add task.
  * @param {object} new task
  * @returns {Promise<Task>} returns the task
  */
-export const createTask = async (task: ITask): Promise<object> => {
+export const createTask = async (task: ITask): Promise<ITask> => {
   DB.tasks.push(task);
   return task;
 };
@@ -162,11 +163,11 @@ export const createTask = async (task: ITask): Promise<object> => {
  * @param {object} edited task
  * @returns {Promise<Task>} edited task
  */
-export const updateTask = async (id: string, modTask: ITask): Promise<object | boolean> => {
+export const updateTask = async (id: string, modTask: ITask): Promise<ITask | null> => {
   const task = DB.tasks.filter(item => item.id === id)[0];
 
   if (!task) {
-    return false;
+    return null;
   }
 
   DB.tasks = DB.tasks.map(item => {
@@ -185,15 +186,15 @@ export const updateTask = async (id: string, modTask: ITask): Promise<object | b
  * @param {string} id the task id
  * @returns {Promise<Task>} returns the deleted task
  */
-export const deleteTask = async (id: string): Promise<object | boolean> => {
-  const forDeletion = DB.tasks.filter(item => item.id === id)[0];
+export const deleteTask = async (id: string): Promise<ITask | null> => {
+  const deletion = DB.tasks.filter(item => item.id === id)[0];
 
-  if (!forDeletion) {
-    return false;
+  if (!deletion) {
+    return null;
   }
 
-  DB.tasks = DB.tasks.filter(task => task.id !== forDeletion.id);
-  return forDeletion;
+  DB.tasks = DB.tasks.filter(task => task.id !== deletion.id);
+  return deletion;
 };
 
 /**
@@ -201,11 +202,8 @@ export const deleteTask = async (id: string): Promise<object | boolean> => {
  * @param {string} the user id
  * @returns {Promise<Task[]>} array tasks
  */
-export const deleteTasksUserId = async (userId: string): Promise<Array<object>> => {
-  DB.tasks = DB.tasks.map(item =>
-    item.userId === userId ? { ...item, userId: null } : { ...item }
-  );
-
+export const deleteTasksUserId = async (userId: string): Promise<ITask[]> => {
+  DB.tasks = DB.tasks.map(item => item.userId === userId ? { ...item, userId: null } : { ...item });
   return DB.tasks;
 };
 
@@ -214,7 +212,7 @@ export const deleteTasksUserId = async (userId: string): Promise<Array<object>> 
  * @param {string} id board id
  * @returns {Promise<Board[]>} array boards
  */
-export const deleteTasksByBoard = async (boardId: string): Promise<Array<object>> => {
+export const deleteTasksByBoard = async (boardId: string): Promise<ITask[]> => {
   DB.tasks = DB.tasks.filter(item => item.boardId !== boardId);
   return DB.tasks;
 };
